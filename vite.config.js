e83de379742e55474016b695b9752e109ebcd3bc
Harvey <harvey0379@163.com>
2021-05-28 14:00:52 +0800

    code split

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import reactRefresh from '@vitejs/plugin-react-refresh'
+import { defineConfig } from 'vite';
+import reactRefresh from '@vitejs/plugin-react-refresh';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh()]
-})
+  plugins: [reactRefresh()],
+  build: {
+    // chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          lodash: ['lodash-es'],
+          antv: ['@antv/x6'],
+          jquery: ['jquery', 'jquery-mousewheel'],
+          mousetrap: ['mousetrap'],
+        },
+      },
+    },
+  },
+});
